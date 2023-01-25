a591e4962bbd4ed45a8fa4c72bb2b4a29618e8ee
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-01-25 13:34:18 -0800

    JWT Auth server

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const router = require('./router/index');
+const errorMiddleware = require('./middleware/error-middleware');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use('/api', router);
+app.use(errorMiddleware);
+
+const start = async () => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose
+      .connect(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+      .then(() => {
+        console.log('Connected to DB!');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+start();
---
+++ service/user-service.js
@@ -0,0 +1,82 @@
+const UserModel = require('../models/user-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const mailService = require('./mail-service');
+const tokenService = require('./token-service');
+const UserDTO = require('../dtos/user-dto');
+const ApiError = require('../exceptions/api-error');
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({email});
+
+    if (candidate) {
+      throw ApiError.BadRequest(`The user with the email address ${email} already exists`)
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const activationLink = uuid.v4();
+    const user = await UserModel.create({email, password: hashPassword, activationLink});
+    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+
+    const userDTO = new UserDTO(user);
+    const tokens = tokenService.generateTokens({...userDTO});
+    await tokenService.saveToken(userDTO.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDTO }
+  }
+
+  async activate(link) {
+    const user = await UserModel.findOne({link});
+    if (!user) {
+      throw ApiError.BadRequest('Bad activation link');
+    }
+    user.isActivated = true;
+    await user.save();
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({email});
+    if (!user) {
+      throw ApiError.BadRequest('The user with this email was not found');
+    }
+    const isPassEqual = await bcrypt.compare(password, user.password);
+    if (!isPassEqual) {
+      throw ApiError.BadRequest('Invalid password');
+    }
+    const userDTO = new UserDTO(user);
+    const tokens = tokenService.generateTokens({...userDTO});
+    await tokenService.saveToken(userDTO.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDTO }
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDB = await tokenService.findToken(refreshToken);
+    if (!userData || !tokenFromDB) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const user = await UserModel.findById(userData.id);
+    const userDTO = new UserDTO(user);
+    const tokens = tokenService.generateTokens({...userDTO});
+    await tokenService.saveToken(userDTO.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDTO }
+  }
+
+  async getAllUsers() {
+    const users = await UserModel.find();
+    return users;
+  }
+}
+
+module.exports = new UserService();
