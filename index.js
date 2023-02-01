a53e52fc70adff1450dd2b6662ad0c2ae613de87
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-01-31 18:56:01 -0800

    fix cors

--- index.js
+++ index.js
@@ -11,10 +11,23 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  credentials: true,
-  origins: process.env.CLIENT_URL
-}));
+const corsOptions = {
+  origin(origin, callback) {
+    callback(null, true);
+  },
+  credentials: true
+};
+app.use(cors(corsOptions));
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+  next();
+}
+app.use(allowCrossDomain);
+
+
+
 app.use('/api', router);
 app.use(errorMiddleware);
 
