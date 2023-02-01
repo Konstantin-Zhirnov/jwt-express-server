3b35347053aec27b47dd8affcbcec39981befe5d
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-01-31 18:04:29 -0800

    cors

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origins: ['http://localhost:300', 'https://mellifluous-lily-395be2.netlify.app/']
+}));
 app.use('/api', router);
 app.use(errorMiddleware);
 
