7f794d693b4874ac5fab4cfdef111e5270edddea
Konstantin Zhirnov <zhirnov-11.06@yandex.ru>
2023-02-01 21:12:44 -0800

    authorization 7

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://mellifluous-lily-395be2.netlify.app/');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
   res.header('Access-Control-Allow-Credentials', true);
