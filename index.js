d1d609ad88462b709730517e148fa233d47b2bc8
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-02-07 17:12:22 -0800

    secure: true

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ const allowCrossDomain = function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Expose-Headers', 'Set-Cookie')
   next();
 }
 app.use(allowCrossDomain);
