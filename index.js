015ad27d631f5f384569b9bf825c782f2d5859d9
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-03-20 21:39:44 -0700

    change address ***

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const router = require('./router/index');
 const errorMiddleware = require('./middleware/error-middleware');
 
 const PORT = process.env.PORT || 80;
-const app = express();
-
+const app = express()
+app.use(cors())
+app.use(express.json());
 
 app.use(cookieParser());
 
-const corsOptions = {
-  origin(origin, callback) {
-    callback(null, true);
-  },
-  credentials: true
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin(origin, callback) {
+//     callback(null, true);
+//   },
+//   credentials: true
+// };
+// app.use(cors(corsOptions));
+//
+// const allowCrossDomain = function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'https://kostya-zhirnov.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// }
+// app.use(allowCrossDomain);
 
-const allowCrossDomain = function(req, res, next) {
-  // res.header('Access-Control-Allow-Origin', 'https://kostya-zhirnov.vercel.app');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,token');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-}
-app.use(allowCrossDomain);
-app.use(express.json());
 app.use('/api', router);
 app.use(errorMiddleware);
 
