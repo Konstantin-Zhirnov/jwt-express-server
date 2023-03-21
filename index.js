c02722e34dee4e3bd2fea97f1aea4ee2cf642687
Konstantin Zhirnov <kostya.zhirnov@gmail.com>
2023-03-20 21:33:10 -0700

    change address *

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://kostya-zhirnov.vercel.app');
+  // res.header('Access-Control-Allow-Origin', 'https://kostya-zhirnov.vercel.app');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type,token');
   res.header('Access-Control-Allow-Credentials', true);
